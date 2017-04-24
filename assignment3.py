8402ff7caf2a4807cb7c06fe37425457b08e7d9c
frankruge <frankruge@gmail.com>
2017-04-24 21:34:08 +0200

    parents and son combined

--- assignment3.py
+++ assignment3.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python2
 
 import vcf
+from vcf import utils #vcf.utils.walk_together doesnt work
 #import hgvs
 __author__ = 'Frank Ruge'
 
@@ -20,17 +21,17 @@ class Assignment3:
         ## Check if hgvs is installed
         #print("HGVS version: %s" % hgvs.__version__)
         #mother
-        self.mother_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24143.vcf"))
-        self.m_records = list([i for i in self.mother_vcf])
-        self.header = self.mother_vcf._header_lines
+        #self.mother_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24143.vcf"))
+        #self.m_records = list([i for i in self.mother_vcf])
+        #self.header = self.mother_vcf._header_lines
         #father
-        self.father_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24149.vcf"))
-        self.f_records = list([i for i in self.father_vcf])
-        self.header = self.father_vcf._header_lines
+        #self.father_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24149.vcf"))
+        #self.f_records = list([i for i in self.father_vcf])
+        #self.header = self.father_vcf._header_lines
         #son
-        self.son_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24385.vcf"))
-        self.s_records = list([i for i in self.son_vcf])
-        self.header = self.son_vcf._header_lines
+        #self.son_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24385.vcf"))
+        #self.s_records = list([i for i in self.son_vcf])
+        #self.header = self.son_vcf._header_lines
 
     def get_total_number_of_variants_mother(self):
         count = 0
@@ -62,84 +63,89 @@ class Assignment3:
        
         
     def get_variants_shared_by_father_and_son(self):
+        self.father_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24149.vcf"))
+        self.son_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24385.vcf"))
+        shared = utils.walk_together(self.father_vcf, self.son_vcf)
         FS_list=[]
-        for i, j in zip(self.f_records, self.s_records):
-
-            if i.CHROM == j.CHROM and i.POS == j.POS and i.REF == j.REF and i.ALT == j.ALT:
+        for i in shared:
+            if i[0] == i[1]:
                 FS_list.append(i)
-                print(i)
             else:
                 pass
         print(len(FS_list))
-        return (FS_list)
-        '''
-        Return the number of identified variants shared by father and son
-        :return: 
-        '''
-        print("TODO")
+        return FS_list
         
 
 
     def get_variants_shared_by_mother_and_son(self):
         MS_list = []
-        for i, j in zip(self.m_records, self.s_records):
-
-            if i.CHROM == j.CHROM and i.POS == j.POS and i.REF == j.REF and i.ALT == j.ALT:
+        self.mother_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24143.vcf"))
+        self.son_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24385.vcf"))
+        shared = utils.walk_together(self.mother_vcf, self.son_vcf)
+        for i in shared:
+            if i[0] == i[1]:
                 MS_list.append(i)
-                print(i)
             else:
                 pass
         print(len(MS_list))
-        return (MS_list)
-        '''
-        Return the number of identified variants shared by mother and son
-        :return: 
-        '''
-        
-    def get_variants_shared_by_trio(self, m, f):
-        #to make it quicker I'll input the results of the mother_son and father_son
-        #basically the same as above
-        trio_list = []
-        for i, j in zip(m, f):
+        return MS_list
 
-            if i.CHROM == j.CHROM and i.POS == j.POS and i.REF == j.REF and i.ALT == j.ALT:
-                trio_list.append(i)
-                print(i)
+    def get_variants_shared_by_trio(self):
+        self.father_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24149.vcf"))
+        self.mother_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24143.vcf"))
+        self.son_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24385.vcf"))
+        shared = utils.walk_together(self.father_vcf, self.son_vcf, self.mother_vcf)
+        FSM_list = []
+        for i in shared:
+            if i[0] == i[1] == i[2]: #makes sure that the variants are the same
+                FSM_list.append(i)
             else:
                 pass
-        print(len(trio_list))
-        return (trio_list)
-
-        '''
-        Return the number of identified variants shared by father, mother and son
-        :return: 
-        '''
-        print("TODO")
+        print(len(FSM_list))
+        return FSM_list
         
 
-    def merge_mother_father_son_into_one_vcf(self, m_s, f_s):
-        file = open("triomerge", 'w')
-        mom = []
-        dad = []
-        son = []
-        for i, j in zip(self.m_records, m_s):
-            if i.CHROM == j.CHROM and i.POS == j.POS and i.REF == j.REF and i.ALT == j.ALT:
-                pass
-            else:
-                MS.append(i)
-        for i, j in zip(MS, f_s):
-            if i.CHROM == j.CHROM and i.POS == j.POS and i.REF == j.REF and i.ALT == j.ALT:
-                pass
-            else:
-                MS.append(i)
-                vcf.Writer(i, , lineterminator='\n')
-        print(len(MS_list))
-        return (MS_list)
+    def merge_mother_father_son_into_one_vcf(self):
+        self.father_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24149.vcf"))
+        self.mother_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24143.vcf"))
+        self.son_vcf = vcf.Reader(open("AmpliseqExome.20141120.NA24385.vcf"))
+        FS = open("FS.vcf", "w")
+        FSVCF = vcf.Writer(FS, self.son_vcf, "\n")
+        shared = utils.walk_together(self.father_vcf, self.son_vcf, self.mother_vcf)
+        count = 0
+        for i in shared:
+            if i[0] or i[1] or i[2]:  #
+                if i[0] is None:
+                    if i[1] is None:
+                        FSVCF.write_record(i[2]);count += 1
+                    elif i[2] is None:
+                        FSVCF.write_record(i[1]);count += 1
+                    else: #if i[2] and i[1] are true
+                        FSVCF.write_record(i[1]);count += 1
+                elif i[1] is None:
+                    if i[0] is None:
+                        FSVCF.write_record(i[2]);count += 1
+                    elif i[2] is None:
+                        FSVCF.write_record(i[0]);count += 1
+                    else: #if i[2] and i[0] are true
+                        FSVCF.write_record(i[2]);count += 1
+                elif i[2] is None:
+                    if i[0] is None:
+                        FSVCF.write_record(i[1]);count += 1
+                    elif i[1] is None:
+                        FSVCF.write_record(i[0]);count += 1
+                    else:  # if i[1] and i[0] are true
+                            FSVCF.write_record(i[0]);count += 1
+                else:
+                    FSVCF.write_record(i[0]);count += 1
+
+        FSVCF.close()
+        print(count) #53227
+        return
         '''
         Creates one VCF containing all variants of the trio (merge VCFs)
         :return: 
         '''
-        print("TODO")
         
 
     def convert_first_variants_of_son_into_HGVS(self):
@@ -160,9 +166,9 @@ class Assignment3:
         #a = assignment1.get_total_number_of_variants_son(); print(a)
         #nr_shared_FS = assignment1.get_variants_shared_by_father_and_son();#print(nr_shared_FS)     #152
         #nr_shared_MS = assignment1.get_variants_shared_by_mother_and_son();#print(nr_shared_MS)   #91
-        #shared_FS = assignment1.get_variants_shared_by_trio(nr_shared_MS, nr_shared_FS);#print(shared_FS) #1   Record(CHROM=chr1, POS=871334, REF=G, ALT=[T])
-
-        assignment1.merge_mother_father_son_into_one_vcf()
+        #shared_FS = assignment1.get_variants_shared_by_trio(); print(shared_FS) #1   Record(CHROM=chr1, POS=871334, REF=G, ALT=[T])
+        merged_File= assignment1.merge_mother_father_son_into_one_vcf()
+        #assignment1.merge_mother_father_son_into_one_vcf()
         #print(len(shared_MS))
 
 
